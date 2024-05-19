259bf4b76fda8082e8373781ea19c18c2df51fff
Marek Migas <migas@nonstop.studio>
2024-05-19 18:04:03 +0200

    matter boilerplate update

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const matterContainer = document.querySelector("#matter-container");
+const THICCNESS = 60;
 
 // module aliases
 var Engine = Matter.Engine,
@@ -18,18 +19,82 @@ var render = Render.create({
         width: matterContainer.clientWidth,
         height: matterContainer.clientHeight,
         background: "transparent",
-        wireframes: true,
-        showAngleIndicator: true
+        wireframes: false,
+        showAngleIndicator: false
     }
 });
 
 // create two boxes and a ground
-var boxA = Bodies.rectangle(400, 200, 80, 80);
-var boxB = Bodies.rectangle(450, 50, 80, 80);
-var ground = Bodies.rectangle(400, 610, 810, 60, { isStatic: true });
+// var boxA = Bodies.rectangle(400, 200, 80, 80);
+// var boxB = Bodies.rectangle(450, 50, 80, 80);
+
+for (let i = 0; i < 5; i++) {
+    let circle = Bodies.circle(i, 10, 30, {
+        friction: 0.3,
+        frictionAir: 0.00001,
+        restitution: 0.8
+    });
+    Composite.add(engine.world, circle);
+}
+
+
+var image = Bodies.rectangle(10, 30, 100, 100, {
+    render: {
+        sprite: {
+            texture: './img/illustration-1/png'
+        }
+    }
+});
+
+Composite.add(engine.world, image);
+
+var ground = Bodies.rectangle(matterContainer.clientWidth / 2, matterContainer.clientHeight + THICCNESS / 2, 27639, THICCNESS, { isStatic: true });
+
+var leftWall = Bodies.rectangle(
+   0 - THICCNESS / 2,
+    matterContainer.clientHeight / 2,
+    THICCNESS,
+    matterContainer.clientHeight * 5,
+    { isStatic: true}
+);
+
+var rightWall = Bodies.rectangle(
+    matterContainer.clientWidth + THICCNESS / 2,
+    matterContainer.clientHeight / 2,
+    THICCNESS,
+    matterContainer.clientHeight * 5,
+    { isStatic: true}
+);
 
 // add all of the bodies to the world
-Composite.add(engine.world, [boxA, boxB, ground]);
+Composite.add(engine.world, [ground, leftWall, rightWall]);
+
+let mouse = Matter.Mouse.create(render.canvas);
+let mouseConstraint = Matter.MouseConstraint.create(
+    engine,
+    {
+        mouse: mouse,
+        constraint: {
+            stiffness: 0.2,
+            render: {
+                visible: false
+            }
+        }
+    }
+);
+
+Composite.add(engine.world, mouseConstraint);
+
+// allow scroll through the canvas
+mouseConstraint.mouse.element.removeEventListener(
+    "mousewheel",
+    mouseConstraint.mouse.mousewheel
+);
+
+mouseConstraint.mouse.element.removeEventListener(
+    "DOMMouseScroll",
+    mouseConstraint.mouse.mousewheel
+);
 
 // run the renderer
 Render.run(render);
@@ -39,3 +104,29 @@ var runner = Runner.create();
 
 // run the engine
 Runner.run(runner, engine);
+
+function handleResize(matterContainer) {
+    // set canvas size to new values
+    render.canvas.width = matterContainer.clientWidth;
+    render.canvas.height = matterContainer.clientHeight;
+
+    // reposition ground
+    Matter.Body.setPosition(
+        ground,
+        Matter.Vector.create(
+            matterContainer.clientWidth / 2,
+            matterContainer.clientHeight + THICCNESS / 2
+        )
+    );
+
+    // reposition right wall
+    Matter.Body.setPosition(
+        rightWall,
+        Matter.Vector.create(
+            matterContainer.clientWidth + THICCNESS / 2,
+            matterContainer.clientHeight / 2
+        )
+    );
+}
+
+window.addEventListener("resize", () => handleResize(matterContainer));
